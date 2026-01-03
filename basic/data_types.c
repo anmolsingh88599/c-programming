37d4540e77795cfa071ebd5ecfa4a5ee0f7e3912
Anmol Singh <anmolsingh88599@gmail.com>
2026-01-03 16:13:29 +0530

    Create data_types.c

---
+++ basic/data_types.c
@@ -0,0 +1,15 @@
+// Program to demonstrate data types
+
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    float b = 5.5;
+    char c = 'A';
+
+    printf("Integer value: %d\n", a);
+    printf("Float value: %.2f\n", b);
+    printf("Character value: %c", c);
+
+    return 0;
+}
