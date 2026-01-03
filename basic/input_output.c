cd9dbaacff81898beaed0f7c28b3a486d9617e0b
Anmol Singh <anmolsingh88599@gmail.com>
2026-01-03 16:06:17 +0530

    Create input_output.c

---
+++ basic/input_output.c
@@ -0,0 +1,13 @@
+// Program to demonstrate input and output
+
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("You entered: %d", num);
+    return 0;
+}
